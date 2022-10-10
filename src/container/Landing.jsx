c8fbf74258fe34126c089a4366ffe613c7ac21b1
pear-aqua <pearlaquagreenblood@gmail.com>
2022-10-10 21:38:55 +0900

    fix margin in langing

--- src/container/Landing.jsx
+++ src/container/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <>
-      <div className="flex flex-col w-[350px] md:w-96 px-8 py-7 mt-36 mb-2 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
+      <div className="flex flex-col w-[350px] md:w-96 px-8 py-7 mt-20 md:mt-36 mb-2 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
         <span className="">Coming Soon!!</span>
       </div>
       <div className="flex flex-col w-[350px] md:w-96 px-8 py-7 mb-2 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
@@ -17,7 +17,7 @@ const Landing = () => {
           disabled
         />
       </div>
-      <div className="flex flex-col w-[350px] md:w-96 mb-36 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
+      <div className="flex flex-col w-[350px] md:w-96 mb-20 md:mb-36 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
         <Link to="/2022-happy-kibum-day">
           <div className="w-full h-full px-8 py-7 ">
             2022 Happy kibum Day! 결과 보러 가기
