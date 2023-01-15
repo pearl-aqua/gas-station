78d65039ffac16e0baa6ae36a3c1c71c1b508f13
pear-aqua <pearlaquagreenblood@gmail.com>
2023-01-15 23:21:49 +0900

    FIX: 랜딩페이지 수정

--- src/container/Landing.jsx
+++ src/container/Landing.jsx
@@ -4,8 +4,8 @@ import Best3 from './Best3';
 const Landing = () => {
   return (
     <>
-      <Best3 />
-      {/* <div className="flex flex-col w-[350px] md:w-96 px-8 py-7 mt-20 md:mt-36 mb-2 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
+      {/* <Best3 /> */}
+      <div className="flex flex-col w-[350px] md:w-96 px-8 py-7 mt-20 md:mt-36 mb-2 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
         <span className="text-lg">Coming Soon!!</span>
       </div>
       <div className="flex flex-col w-[350px] md:w-96 mb-20 md:mb-36 bg-white rounded-2xl border border-slate-200 text-slate-400 z-50">
@@ -14,7 +14,7 @@ const Landing = () => {
             2022 Happy kibum Day! 결과 보러 가기
           </div>
         </Link>
-      </div> */}
+      </div>
     </>
   );
 };
